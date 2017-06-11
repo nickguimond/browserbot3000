c713007ffbdb16e36145aaa6d8902a786de32dfe
Nick Guimond <nicholas.guimond@outlook.com>
2017-06-10 23:31:17 -0300

    multibrowser support

--- boilerplate/spec.js
+++ boilerplate/spec.js
@@ -1,10 +1,35 @@
 process.env.IP = '127.0.0.1';  // Set the location of you running selenium server or chromedriver instance.
 process.env.PORT = '4444';  // Set the port your server is listening on. 
+// process.env.EDGE = 'true'; // Set this if you want to use EDGE browser
 const { browser } = require('browserbot3000'); // You can include the browser here or only include it in your page objects reusable methods
 
-let Google = require('./po');  // Add your page object
+let Google = require('./page');  // Add your page object
 
-browser.newChromeSession();
+// let firefox = {
+// 	'desiredCapabilities': {
+// 		'browserName': 'firefox',
+// 		"moz:firefoxOptions": {
+// 			"args": ["--no-remote"],
+// 			"prefs": {
+// 				"dom.ipc.processCount": 8
+// 			},
+// 			"log": {
+// 				"level": "trace"
+// 			}
+// 		}
+// 	}
+// };
+// let edge = {
+// 	'desiredCapabilities': {
+// 		'browserName': 'edge'
+// 	}
+// };
+
+
+browser.newBrowserSession(); //or using the above objects...   browser.newBrowserSession(edge);   browser.newBrowserSession(firefox);
 browser.url('http://google.com');
 Google.searchBox.setValue('first search term' + '\uE007'); // use a page element directly 
 Google.googleSearch('SeleniumHQ'); // or construct reusable methods for repeatable actions
+
+browser.close();
+browser.deleteCurrentSession();
